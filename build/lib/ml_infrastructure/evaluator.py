bug fix for slope calculation

build/lib/ml_infrastructure/evaluator.py
@@ -122,8 +122,10 @@ class Evaluator:
             performance = calc_performance(matrix, self.model, self.data_manager.classes, mode)
             send_performance(performance, self.ip, self.port)
 
+            total_performance[mode] = performance
+
         self.model.save(mode="current")
 
-        if total_performance['validation']['F1-Score'] > self.best_f1:
+        if np.mean(total_performance['validation']['F1-Score']) > self.best_f1:
             self.model.save(mode='best')
-            self.best_f1 = total_performance['validation']['F1-Score']
+            self.best_f1 = np.mean(total_performance['validation']['F1-Score'])

build/lib/ml_infrastructure/manager.py
@@ -41,16 +41,19 @@ class Manager:
         requests.post(url=url, json=post_body)
 
     def perform(self):
-        validation_loss = deque(maxlen=self.window_size)
+
         for trainer, evaluator in zip(self.trainers, self.evaluators):
             while True:
                 loss = trainer.train()
                 evaluator.evaluate()
 
-                validation_loss.append(loss['mean_validation_loss'])
+                validation_loss = deque(maxlen=self.window_size)
 
-                if len(validation_loss) > 2:
+                [validation_loss.append(l) for l in loss['validation_loss']]
+                print(validation_loss)
+                if len(validation_loss) == self.window_size:
                     slope = np.polyfit(range(0, len(validation_loss)), validation_loss, 1)[0]
+                    print(slope)
                     if -0.1 < slope:
                         trainer.model.save(mode="Final")
                         break

ml_infrastructure/trainer.py
@@ -12,12 +12,12 @@ class Trainer:
     ip: str = "0.0.0.0"
     port: int = 5000
     epochs: int = 1
+    train_loss = []
+    val_loss = []
 
     def train(self):
         train_loader = self.data_manager.train_loader
         val_loader = self.data_manager.validation_loader
-        train_loss = []
-        val_loss = []
 
         for epoch in range(self.epochs):
             loss = []
@@ -26,9 +26,8 @@ class Trainer:
                 target = data_target[1]
                 loss.append(self.model.step(data, target))
             epoch_loss = np.mean(loss)
-            train_loss.append(epoch_loss)
-            self.send_watcher('loss', 'training', epoch_loss, len(train_loss))
-
+            self.train_loss.append(epoch_loss)
+            self.send_watcher('loss', 'training', epoch_loss, len(self.train_loss))
             with torch.no_grad():
                 loss = []
                 for data_target in val_loader:
@@ -36,18 +35,17 @@ class Trainer:
                     target = data_target[1]
                     loss.append(self.model.loss(data, target))
             epoch_loss = np.mean(loss)
-            val_loss.append(epoch_loss)
-            self.send_watcher('loss', 'validation', epoch_loss, len(val_loss))
+            self.val_loss.append(epoch_loss)
+            self.send_watcher('loss', 'validation', epoch_loss, len(self.val_loss))
 
-            return {
-                'mean_train_loss': np.mean(train_loss),
-                'mean_validation_loss': np.mean(val_loss),
-            }
+        return {
+            'train_loss': self.train_loss,
+            'validation_loss': self.val_loss,
+        }
 
     def send_watcher(self, metric, mode, value, idx):
         server = f'http://{self.ip}:{self.port}/'
         endpoint = ""
-
         if metric == 'loss':
             endpoint = "updateLoss"
 